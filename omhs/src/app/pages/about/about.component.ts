f4c0b2cc6b96695415a09f98afd776ba44140469
Dan Hoffman <dghoffman@gmail.com>
2018-08-15 15:03:53 -0400

    added about page component.

--- omhs/src/app/pages/about/about.component.ts
+++ omhs/src/app/pages/about/about.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { fromEvent } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+declare var $:any;
 
 @Component({
   selector: 'app-about',
@@ -9,7 +13,26 @@ export class AboutComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  screenWidth;
+  sizeMobile = true;
 
+  ngOnInit() {
+    this.screenWidth = window.innerWidth;
+    if (this.screenWidth > 669) {
+      this.sizeMobile = false;
+    }
+
+    fromEvent(window, 'resize')
+      .pipe(
+        map(() => window.innerWidth)
+      )
+      .subscribe(
+        windowWidth => {
+          this.screenWidth = windowWidth;
+          this.sizeMobile = (windowWidth > 669) ? false : true;
+        },
+        error => console.log(error),
+        () => console.log('Completed')
+      );
+  }
 }
