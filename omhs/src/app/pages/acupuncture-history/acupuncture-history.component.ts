f97eab837af512d3c7d06718a5add08a4cf741c2
Dan Hoffman <dghoffman@gmail.com>
2018-08-23 10:06:10 -0400

    added scroll from top data and updated method to display the tool tip above or below the link as appropriate.

--- omhs/src/app/pages/acupuncture-history/acupuncture-history.component.ts
+++ omhs/src/app/pages/acupuncture-history/acupuncture-history.component.ts
@@ -17,7 +17,7 @@ export class AcupunctureHistoryComponent implements OnInit {
   sizeMobile = true;
   activeMainMenu = $('#acupuncture');
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.screenWidth = window.innerWidth;
     if (this.screenWidth > 669) {
       this.sizeMobile = false;
@@ -37,44 +37,46 @@ export class AcupunctureHistoryComponent implements OnInit {
         () => console.log('Completed')
       );
 
-      $('.tooltip').mouseover(function(e){
+      $('.tooltip')
+      .mouseover(function(e){
         if( $(this).attr('data-tip-type') == 'text' ){
           $('#tooltip-container').html($(this).attr('data-tip-source'));
         }
 
         if( $(this).attr('data-tip-type') == 'html' ){
-          let elementToGet = '#'+ $(this).attr('data-tip-source')
+          let elementToGet = '#'+ $(this).attr('data-tip-source');
           let newHTML = $(elementToGet).html();
           $('#tooltip-container').html(newHTML);
         }
 
         $('#tooltip-container').css({'display': 'block', 'opacity':0}).animate({opacity:1}, 300);
 
-      }).mousemove(function(e){
+      })
+      .mousemove(function(e){
+
         let toolTipWidth = $('#tooltip-container').outerWidth();
         let toolTipHeight = $('#tooltip-container').outerHeight();
-
         let pageWidth = $('body').width();
+        let scroll = $(window).scrollTop();
+
         if ( e.pageX > pageWidth/2 ){
           $('#tooltip-container').css('left', (e.pageX-toolTipWidth+20)+'px');
         }else{
           $('#tooltip-container').css('left', (e.pageX-20)+'px');
         }
 
-        if(e.pageY > 100){
+        if( e.pageY > (scroll + toolTipHeight) ){
           $('#tooltip-container').css('top', (e.pageY-(toolTipHeight+20))+'px');
         }else{
-          $('#tooltip-container').css('top', (e.pageY+20)+'px');
+           $('#tooltip-container').css('top', (e.pageY+20)+'px');
         }
 
-        
-      }).mouseout(function(e){
-       
+      })
+      .mouseout(function(e){
+
         $('#tooltip-container').animate({opacity:0}, 250, function(){
           $('#tooltip-container').css('display', 'none').html('');
-
         });
-       
 
       });
   }
